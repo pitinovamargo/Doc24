6c33a5d75e82b6c11aeb5ee2fd579af45c9df4fa
Margarita <pitinovamargo@gmail.com>
2024-07-05 15:43:03 +0200

    Initial Commit

---
+++ Doc24/Doc24App.swift
@@ -0,0 +1,17 @@
+//
+//  Doc24App.swift
+//  Doc24
+//
+//  Created by Margarita Pitinova on 05.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct Doc24App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
